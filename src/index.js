4773ed390ab5e5ad5415ec5fa9c22549d5fe36ba
Dua Tariq <duatariq243@gmail.com>
2025-12-22 20:10:06 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/product-cult-react">
     <App />
     
   </BrowserRouter>
